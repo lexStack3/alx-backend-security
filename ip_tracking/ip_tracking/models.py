4e8d873631c62b3758b8bba3c5b1facd1aaa92c1
lexStack3 <alexanderedim80@gmail.com>
2026-01-28 21:58:07 +0100

    Basic IP Logging Middleware

---
+++ ip_tracking/ip_tracking/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class RequestLog(models.Model):
+    """Model representation of a <RequestLog> instance."""
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=255)
+
+    def __str__(self):
+        """String representation of a <RequestLog> instance."""
+        return f"IP: {self.ip_address}"
+
