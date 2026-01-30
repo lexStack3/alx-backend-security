f12106330274b2c893ee0b2f43f475a08e789713
lexStack3 <alexanderedim80@gmail.com>
2026-01-30 22:26:48 +0100

    Added fields: 'country' & 'city' for the Requesting model.

--- ip_tracking/ip_tracking/models.py
+++ ip_tracking/ip_tracking/models.py
@@ -7,6 +7,9 @@ class RequestLog(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     path = models.CharField(max_length=255)
 
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+
     def __str__(self):
         """String representation of a <RequestLog> instance."""
         return f"IP: {self.ip_address}"
@@ -15,6 +18,7 @@ class RequestLog(models.Model):
 class BlockIP(models.Model):
     """Model representation of a <BlockIP> instance."""
     ip_address = models.GenericIPAddressField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """String representation of a <BlockIP> instance"""
